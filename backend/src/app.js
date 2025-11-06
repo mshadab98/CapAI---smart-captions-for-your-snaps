c110bb8a7a8989b0d4cf2c59962e147e420078e2
mshadab98 <mohdshadab98977@gmail.com>
2025-11-06 20:29:41 +0530

    origin set of frontend parts

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser')
 
 const app = express()
 app.use(cors({
-   origin: "http://localhost:5173",
+   origin: "https://cap-ai-smart-captions-for-your-snap-chi.vercel.app/",
    credentials: true
 }));
 app.use(cookieParser())
