bd858a3a1bc04d68ad46aa5954e9f2d88adc8dae
mshadab98 <mohdshadab98977@gmail.com>
2025-11-06 18:24:37 +0530

    changes in the file

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
-require('dotenv').config()
-const app = require('./src/app')
-const connectDB = require('./src/db/db')
+// require('dotenv').config()
+// const app = require('./src/app')
+// const connectDB = require('./src/db/db')
 
 
-connectDB()
+// connectDB()
 
 
-app.listen(5000,()=>{
-    console.log('server is running on 5000')
-})
+// app.listen(5000,()=>{
+//     console.log('server is running on 5000')
+// })
+
+
+require('dotenv').config();
+const app = require('./src/app');
+const connectDB = require('./src/db/db');
+
+connectDB();
+
+module.exports = app;  // 👈 yahi important hai
